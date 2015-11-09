5299abaf08bf3937abd159b9677c41d929eb66ab
ttitto <ttitto_prog@vfemail.net>
2015-11-09 23:52:05 +0200

    BaseController added

---
+++ MVCTemplateUoWAutomapperNinject/MvcUoWAutomNin/MvcUoWAutomNin.MVC/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using MvcUoWAutomNin.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUoWAutomNin.MVC.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        IMvcUoWAutomNinData mvcUoWAutomNinData;
+
+        public BaseController (IMvcUoWAutomNinData mvcUoWAutomNinData)
+        {
+            this.mvcUoWAutomNinData = mvcUoWAutomNinData;
+        }
+    }
+}
