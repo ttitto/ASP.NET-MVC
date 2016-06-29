0b48dc393a5299ecb5278a73323f4b809523963d
ttitto <ttitto.ms@gmail.com>
2016-06-29 23:59:27 +0300

    added MVC middleware and home controller to test

---
+++ OdeToFood/src/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿namespace OdeToFood.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from Controller index action.";
+        }
+    }
+}
--- OdeToFood/src/OdeToFood/Startup.cs
+++ OdeToFood/src/OdeToFood/Startup.cs
@@ -24,6 +24,7 @@ namespace OdeToFood
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
             services.AddSingleton(provider => this.Configuration);
             services.AddSingleton<IGreeter, Greeter>();
         }
@@ -40,6 +41,7 @@ namespace OdeToFood
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvcWithDefaultRoute();
             app.UseFileServer();
 
             // both below are replaced with UseFileServer() in the correct order
