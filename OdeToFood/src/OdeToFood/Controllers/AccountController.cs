a0497f25ebc99e2f596f8e59e45eab4db28bc24e
ttitto <ttitto.ms@gmail.com>
2016-07-11 00:10:59 +0300

    implemented User registration

---
+++ OdeToFood/src/OdeToFood/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿namespace OdeToFood.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels;
+
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public ViewResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = new User() { UserName = model.Username };
+                var result = await this.userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await this.signInManager.SignInAsync(user, false);
+                    return this.RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        this.ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            return this.View();
+        }
+    }
+}
