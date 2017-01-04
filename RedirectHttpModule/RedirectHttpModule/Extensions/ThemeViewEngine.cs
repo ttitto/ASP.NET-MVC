255b32702039c569c591f5edc6a509d2b7340aea
Todor Todorov <ttitto_prog@vfemail.net>
2017-01-04 22:48:40 +0200

    RedirectModule: custom ViewEngine implemented

---
+++ RedirectHttpModule/RedirectHttpModule/Extensions/ThemeViewEngine.cs
@@ -0,0 +1,13 @@
+﻿namespace RedirectHttpModule.Extensions
+{
+    using System.Web.Mvc;
+
+    public class ThemeViewEngine : RazorViewEngine
+    {
+        public ThemeViewEngine()
+        {
+            this.ViewLocationFormats = new string[] { "~/Theme/{1}/{0}.cshtml" };
+            this.PartialViewLocationFormats = new string[] { "~/Theme/{1}/{0}.cshtml" };
+        }
+    }
+}
