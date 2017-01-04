a288f7a2f665024f710ef6ce27ffcaa6ce3861a7
Todor Todorov <ttitto_prog@vfemail.net>
2017-01-04 22:33:32 +0200

    RedirectModule: custom action result implemented

--- RedirectHttpModule/RedirectHttpModule/Controllers/HomeController.cs
+++ RedirectHttpModule/RedirectHttpModule/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace RedirectHttpModule.Controllers
+﻿namespace RedirectHttpModule.Controllers
 {
+    using System.Web.Mvc;
+    using Extensions;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -24,7 +21,13 @@ namespace RedirectHttpModule.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return JsonNet(ViewBag.Message);
+            //return View();
+        }
+
+        private ActionResult JsonNet(object data)
+        {
+            return new JsonNetResult() { Data = data };
         }
     }
 }
---
+++ RedirectHttpModule/RedirectHttpModule/Extensions/JsonNetResult.cs
@@ -0,0 +1,17 @@
+﻿namespace RedirectHttpModule.Extensions
+{
+    using System.Web.Mvc;
+    using Newtonsoft.Json;
+
+    public class JsonNetResult : ActionResult
+    {
+        public object Data { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.ContentType = "application/json";
+            response.Write(JsonConvert.SerializeObject(this.Data));
+        }
+    }
+}
