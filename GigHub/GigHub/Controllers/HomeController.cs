44e527aa88de3cb7b92295755d95245f50b97f96
Todor Todorov <ttitto_prog@vfemail.net>
2017-02-02 09:27:32 +0200

    GigHub: render actions buttons on the home page depending on the logged in user

--- GigHub/GigHub/Controllers/HomeController.cs
+++ GigHub/GigHub/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using GigHub.Models;
+using GigHub.ViewModels;
 
 namespace GigHub.Controllers
 {
@@ -22,7 +23,13 @@ namespace GigHub.Controllers
                 .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
 
-            return View(upcomingGigs);
+            var viewModel = new HomeViewModel()
+            {
+                UpcommingGigs = upcomingGigs,
+                ShowActions = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ GigHub/GigHub/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GigHub.ViewModels
+{
+    using System.Collections.Generic;
+    using Models;
+
+    public class HomeViewModel
+    {
+        public bool ShowActions { get; set; }
+        public IEnumerable<Gig> UpcommingGigs { get; set; }
+    }
+}
