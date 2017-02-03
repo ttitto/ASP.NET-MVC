f5f9c579424ee27421c922f82fee12cd4bae49dc
Todor Todorov <ttitto_prog@vfemail.net>
2017-02-03 21:35:17 +0200

    GigHub: Who I Follow implemented

---
+++ GigHub/GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,28 @@
+﻿namespace GigHub.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+    using Microsoft.AspNet.Identity;
+    using Models;
+
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public FolloweesController()
+        {
+            this.context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = this.context.Follows
+                .Where(follow => follow.FollowerId == userId)
+                .Select(follow => follow.Followed)
+                .ToList();
+
+            return View(artists);
+        }
+    }
+}
---
+++ GigHub/GigHub/ViewModels/GigsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GigHub.ViewModels
+{
+    using System.Collections.Generic;
+    using Models;
+
+    public class GigsViewModel
+    {
+        public string Heading { get; set; }
+        public bool ShowActions { get; set; }
+        public IEnumerable<Gig> UpcommingGigs { get; set; }
+    }
+}
